fd18fbc236496c5561a99ec2c0004e6abea91b12
basq1231 <basq1231@gmail.com>
2015-03-08 22:23:58 -0400

    Created BrackIT project
    Started storyboard and data model

---
+++ BrackIT/BrackIT/Tournament.swift
@@ -0,0 +1,21 @@
+//
+//  Tournament.swift
+//  BrackIT
+//
+//  Created by Michael Basquill on 3/8/15.
+//  Copyright (c) 2015 Michael Basquill. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc(Tournament)
+class Tournament: NSManagedObject {
+
+    @NSManaged var name: String
+    @NSManaged var createdDate: NSDate
+    @NSManaged var teamCount: NSNumber
+    @NSManaged var teamPlayerCount: NSNumber
+    @NSManaged var autoCreateTeams: NSNumber
+
+}
